c24a5287b6683055941f14d4c756f6b50fbc565c
Juan G <86728893+JuanOnly@users.noreply.github.com>
2021-11-28 16:22:50 -0500

    first commit

---
+++ environment-token.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 25 10:52:03 2021
+@author: Jota
+"""
+
+import os
+
+os.environ["JWT_SECRET_KEY"]="Jwt@Tokens*"
---
+++ servidor-token.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 25 10:42:10 2021
+
+@author: Jota
+"""
+
+
+from flask import Flask
+import os
+from flask import request
+from jose import jwt
+
+app = Flask(__name__)
+os.environ["JWT_SECRET_KEY"]="Jwt@Tokens*"
+JWT_SECRET_KEY = "Jwt@Tokens*"
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World Tokens!</p>"
+
+
+@app.route("/crear-token")
+def crear():
+    nombre = request.args.get("nombre")
+    id_persona = request.args.get("id")
+    id_rol = request.args.get("id_rol")
+    try:
+        secret_key = "Jwt@Tokens*"
+        token = jwt.encode({'nombre': nombre, 'id': id_persona, 'rol': id_rol}, secret_key, algorithm='HS256')
+        return token
+    except Exception as e:
+        return ""
+
+@app.route("/validar-token")
+def validar():
+    token = request.args.get("token")
+    try:
+        secret_key = "Jwt@Tokens*"
+        jwt.decode(token, secret_key, algorithms=['HS256'])
+        return "OK"
+    except Exception as e:
+        return "KO"
+if __name__ == '__main__':
+    app.run(host="localhost", port=5001)
